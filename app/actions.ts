631ad978b403ba70e71ed4c02b024e193eff4469
Ianszzkieyyy <ian.agustin071729@gmail.com>
2026-01-11 23:51:53 +0800

    fixed VAPID errors

--- app/actions.ts
+++ app/actions.ts
@@ -3,7 +3,7 @@
 import webpush, { PushSubscription as WebPushSubscription } from 'web-push'
  
 webpush.setVapidDetails(
-  `<mailto:${process.env.NEXT_PUBLIC_VAPID_CONTACT_EMAIL}>`,
+  `mailto:${process.env.NEXT_PUBLIC_VAPID_CONTACT_EMAIL}`,
   process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY!,
   process.env.VAPID_PRIVATE_KEY!
 )
